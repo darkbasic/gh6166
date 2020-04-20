c9f23c51ccc9438252ae2b4051b1125623d0b0a1
Niccolò Belli <niccolo.belli@linuxsystems.it>
2020-04-20 12:20:50 +0200

    apollo-client github issue 6166 repro

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from './App';
+import Articles from './Articles';
 
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
+  const { getByText } = render(<Articles />);
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
